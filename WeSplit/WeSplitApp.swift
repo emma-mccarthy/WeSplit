4c885c3843ea491c0b5edc57ada2e6d43415dbc2
emma-mccarthy <104154017+emma-mccarthy@users.noreply.github.com>
2022-04-22 12:42:58 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Emma McCarthy on 18/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
